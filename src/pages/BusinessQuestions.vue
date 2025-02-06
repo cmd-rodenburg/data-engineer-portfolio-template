4be0af3cd61b9b51d3f096912459cab19146bf13
Anne Rodenburg <rodenburg.bi@gmail.com>
2025-02-06 17:50:14 +0100

    Added header

--- src/pages/BusinessQuestions.vue
+++ src/pages/BusinessQuestions.vue
@@ -2,11 +2,13 @@
 import SolutionsSection from '../components/Sections/Business/SolutionsSection.vue'
 import PersonalisedSection from '../components/Sections/Business/PersonalisedSection.vue'
 import NavigationBar from '../components/_NavigationBar.vue'
+import Header from '../components/Header.vue'
 </script>
 
 <template>
   <div class="min-h-screen">
     <NavigationBar />
+    <Header imageSrc="/images/Background_contact.jpg" />
     <main class="main-content">
       <SolutionsSection />
       <PersonalisedSection />
